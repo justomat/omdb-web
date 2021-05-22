d677532d18382b5e43de7f386c3b42a93dc675cb
Geraldi Sutanto <contact@geraldisutanto.com>
2021-05-22 15:27:55 +0700

    init redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-
+import { Provider } from "react-redux";
+import store from "../src/redux/store";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>;
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+import { combineReducers, createStore } from "redux";
+
+const root = combineReducers({});
+
+export default createStore(root);
