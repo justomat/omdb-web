081ddeefbce42c8a2c1a5733cbccfd576b140634
Geraldi Sutanto <contact@geraldisutanto.com>
2021-05-23 15:08:03 +0700

    implement search

--- pages/search.js
+++ pages/search.js
@@ -1,30 +1,34 @@
 import Head from "next/head";
 import { useCallback, useRef } from "react";
+import { useRouter } from "next/router";
 import MovieList from "../src/components/MovieList";
 
-export default function Home() {
+export default function SearchPage() {
   const input = useRef(null);
   const search = useCallback((e) => {
     e.preventDefault();
 
     const query = input.current.value;
-    if (query) window.location.href = `/search?s=${query}`;
+    if (query) window.location.href = `/search?id=${query}`;
   }, []);
+
+  const router = useRouter();
+  const s = router.query.s;
 
   return (
     <>
       <Head>
-        <title>Index</title>
+        <title>Search</title>
       </Head>
 
       <nav style={{ display: "inline", flexDirection: "row" }}>
-        <h1>Home</h1>
+        <h1>Search result for '{s}'</h1>
         <input ref={input} />
         <button onClick={search}>search</button>
       </nav>
 
       <main>
-        <MovieList keyword="Batman" />
+        <MovieList keyword={s} />
       </main>
 
       <footer></footer>
