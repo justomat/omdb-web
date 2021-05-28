d47c0d53827c98b0f12f6f0d18bd5af4c4dccb75
Geraldi Sutanto <contact@geraldisutanto.com>
2021-05-28 14:42:43 +0700

    remove deadcode

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,8 @@
 import Head from "next/head";
-import { useCallback, useRef } from "react";
 import MovieList from "../src/components/MovieList";
 import SearchForm from "../src/components/SearchForm";
 
 export default function Home() {
-  const input = useRef(null);
-  const search = useCallback((e) => {
-    e.preventDefault();
-
-    const query = input.current.value;
-    if (query) window.location.href = `/search?s=${query}`;
-  }, []);
-
   return (
     <>
       <Head>
